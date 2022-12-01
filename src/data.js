725c21e27ae9e9634f09837a92138b96fae17b12
jedolce <juandolce20@gmail.com>
2022-12-01 20:25:56 -0300

    commit 7

--- src/data.js
+++ src/data.js
@@ -5,6 +5,9 @@ export const scopeData = [
         type: "text",
         placeholder: "Consumo de Gas Natural en m3",
         label: "Gas Natural",
+        co2: 2.01193,
+        ch4: 0.00274,
+        n2o: 0.00107
     },
     {
         id: 2,
@@ -12,6 +15,9 @@ export const scopeData = [
         type: "text",
         placeholder: "Consumo de Gasoil en L",
         label: "Gasoil",
+        co2: 2.52058,
+        ch4: 0.00026,
+        n2o: 0.03700
     },
     {
         id: 3,
@@ -19,6 +25,9 @@ export const scopeData = [
         type: "text",
         placeholder: "Consumo de Nafta en L",
         label: "Nafta",
+        co2: 2.14805,
+        ch4: 0.00720,
+        n2o: 0.00660
     },
     {
         id: 4,
@@ -26,6 +35,9 @@ export const scopeData = [
         type: "text",
         placeholder: "Consumo de Fueloil en L",
         label: "Fueloil",
+        co2: 3.16262,
+        ch4: 0.00473,
+        n2o: 0.00788
     },
     {
         id: 5,
@@ -33,6 +45,9 @@ export const scopeData = [
         type: "text",
         placeholder: "Consumo de LPG en kg",
         label: "LPG",
+        co2: 2.93518,
+        ch4: 0.00228,
+        n2o: 0.00183
     },
     {
         id: 6,
@@ -40,6 +55,7 @@ export const scopeData = [
         type: "text",
         placeholder: "Refill R22 en Kg",
         label: "R22",
+        co2: 1810,
     },
     {
         id: 7,
@@ -47,6 +63,7 @@ export const scopeData = [
         type: "text",
         placeholder: "Refill R134 en Kg",
         label: "R134",
+        co2: 1100
     },
     {
         id: 8,
@@ -54,6 +71,7 @@ export const scopeData = [
         type: "text",
         placeholder: "Refill R407c en Kg",
         label: "R407c",
+        co2: 1774
     },
     {
         id: 9,
@@ -61,6 +79,7 @@ export const scopeData = [
         type: "text",
         placeholder: "Refill R410a en Kg",
         label: "R410a",
+        co2: 2088
     },
     {
         id: 10,
@@ -68,6 +87,7 @@ export const scopeData = [
         type: "text",
         placeholder: "Refill R141b en Kg",
         label: "R141b ",
+        co2: 725
     },
     {
         id: 11,
@@ -75,6 +95,9 @@ export const scopeData = [
         type: "text",
         placeholder: "Consumo Electrico en kWh",
         label: "Electricidad",
+        co2: 0.19121,
+        ch4: 0.00080,
+        n2o: 0.00137
     },
     {
         id: 12,
@@ -82,6 +105,9 @@ export const scopeData = [
         type: "text",
         placeholder: "Consumo de Vapor en m3",
         label: "Vapor",
+        co2: 1,
+        ch4: 0,
+        n2o: 0
     },
     {
         id: 13,
@@ -89,6 +115,7 @@ export const scopeData = [
         type: "text",
         placeholder: "Horas de Home Office",
         label: "Home Office",
+        co2: 0.34075,
     },
     {
         id: 14,
@@ -162,173 +189,13 @@ export const scopeData = [
     },
 ]
 
+export const constantes = [
+    {
 
-// export const scopeOneData = [
-//     {
-//         id: 1,
-//         name: "gas",
-//         type: "text",
-//         placeholder: "Consumo de Gas Natural en m3",
-//         label: "Gas Natural",
-//     },
-//     {
-//         id: 2,
-//         name: "gasoil",
-//         type: "text",
-//         placeholder: "Consumo de Gasoil en L",
-//         label: "Gasoil",
-//     },
-//     {
-//         id: 3,
-//         name: "nafta",
-//         type: "text",
-//         placeholder: "Consumo de Nafta en L",
-//         label: "Nafta",
-//     },
-//     {
-//         id: 4,
-//         name: "fueloil",
-//         type: "text",
-//         placeholder: "Consumo de Fueloil en L",
-//         label: "Fueloil",
-//     },
-//     {
-//         id: 5,
-//         name: "lpg",
-//         type: "text",
-//         placeholder: "Consumo de LPG en kg",
-//         label: "LPG",
-//     },
-//     {
-//         id: 6,
-//         name: "r22",
-//         type: "text",
-//         placeholder: "Refill R22 en Kg",
-//         label: "R22",
-//     },
-//     {
-//         id: 7,
-//         name: "r134",
-//         type: "text",
-//         placeholder: "Refill R134 en Kg",
-//         label: "R134",
-//     },
-//     {
-//         id: 8,
-//         name: "r407c",
-//         type: "text",
-//         placeholder: "Refill R407c en Kg",
-//         label: "R407c",
-//     },
-//     {
-//         id: 9,
-//         name: "r410a",
-//         type: "text",
-//         placeholder: "Refill R410a en Kg",
-//         label: "R410a",
-//     },
-//     {
-//         id: 10,
-//         name: "r141b",
-//         type: "text",
-//         placeholder: "Refill R141b en Kg",
-//         label: "R141b ",
-//     },
-// ];
+    },
+    {
 
-// export const scopeTwoData = [
-//     {
-//         id: 11,
-//         name: "electricidad",
-//         type: "text",
-//         placeholder: "Consumo Electrico en kWh",
-//         label: "Electricidad",
-//     },
-//     {
-//         id: 12,
-//         name: "vapor",
-//         type: "text",
-//         placeholder: "Consumo de Vapor en m3",
-//         label: "Vapor",
-//     }
-// ];
+    }
+]
 
-// export const scopeThreeData = [
-//     {
-//         id: 13,
-//         name: "home",
-//         type: "text",
-//         placeholder: "Horas de Home Office",
-//         label: "Home Office",
-//     },
-//     {
-//         id: 14,
-//         name: "traslados",
-//         type: "text",
-//         placeholder: "Consumo de Nafta por Transporte Privado",
-//         label: "Transporte Privado",
-//     },
-//     {
-//         id: 15,
-//         name: "publico",
-//         type: "text",
-//         placeholder: "km Recorridos en Transporte Publico",
-//         label: "Transporte Publico",
-//     },
-//     {
-//         id: 16,
-//         name: "cabotaje",
-//         type: "text",
-//         placeholder: "km Recorridos en Vuelos de Cabojate",
-//         label: "Vuelos Cabojate",
-//     },
-//     {
-//         id: 17,
-//         name: "internacional",
-//         type: "text",
-//         placeholder: "km Recorridos en Vuelos Internacionales",
-//         label: "Vuelos Internacionales",
-//     },
-//     {
-//         id: 18,
-//         name: "alojamiento",
-//         type: "text",
-//         placeholder: "Noches de Hotel",
-//         label: "Alojamiento",
-//     },
-//     {
-//         id: 19,
-//         name: "terrestre",
-//         type: "text",
-//         placeholder: "km de Flete Terrestre",
-//         label: "Flete Terrestre",
-//     },
-//     {
-//         id: 20,
-//         name: "maritimo",
-//         type: "text",
-//         placeholder: "km de Flete Marítimo/Aereo",
-//         label: "Flete Marítimo/Aereo",
-//     },
-//     {
-//         id: 21,
-//         name: "agua",
-//         type: "text",
-//         placeholder: "m3 de Agua Consumida",
-//         label: "Agua Corriente",
-//     },
-//     {
-//         id: 22,
-//         name: "tratamiento",
-//         type: "text",
-//         placeholder: "m3 de Agua Tratada",
-//         label: "Agua Tratada",
-//     },
-//     {
-//         id: 23,
-//         name: "residuos",
-//         type: "text",
-//         placeholder: "kg de Residuos",
-//         label: "Residuos",
-//     },
-// ];
+export const gasEnergy = 12;
--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -2,13 +2,102 @@ import React from 'react';
 import PieChart from '../components/PieChart';
 import BarChart from '../components/BarChart';
 import { useLocation } from 'react-router-dom';
+import { scopeData } from '../data';
 
 export const Results = () => {
     const location = useLocation();
+    const newState = location.state.scopeState;
 
-    console.log(location.state);
-    console.log(location.state.scopeState.gas);
-    const data = location.state.scopeState.gas * 2;
+    // Gas
+    const gasState = newState.gas;
+    const gasEf = scopeData[0];
+    const gasConst = [gasEf.co2, gasEf.ch4, gasEf.n2o];
+    const gasResult = [];
+
+    gasConst.map((item) => {
+        return gasResult.push(Math.round((item * gasState) * 1000) / 1000)
+    });
+
+    const gasTotal = gasResult.reduce((acc, val) => {
+        return acc + val
+    }, 0);
+
+    console.log(gasResult);
+    console.log(gasTotal);
+
+    // Gasoil
+    const gasoilEf = scopeData[1];
+    const gasoilConst = [gasoilEf.co2, gasoilEf.ch4, gasoilEf.n2o];
+    const gasoilState = newState.gasoil;
+    const gasoilResult = [];
+
+    gasoilConst.map((item) => {
+        return gasoilResult.push(Math.round((item * gasoilState) * 1000) / 1000)
+    });
+
+    const gasoilTotal = gasoilResult.reduce((acc, val) => {
+        return acc + val
+    }, 0);
+
+    console.log(gasoilResult);
+    console.log(gasoilTotal);
+
+    // Nafta
+    const naftaEf = scopeData[2];
+    const naftaConst = [naftaEf.co2, naftaEf.ch4, naftaEf.n2o];
+    const naftaState = newState.nafta;
+    const naftaResult = [];
+
+    naftaConst.map((item) => {
+        return naftaResult.push(Math.round((item * naftaState) * 1000) / 1000)
+    });
+
+    const naftaTotal = naftaResult.reduce((acc, val) => {
+        return acc + val
+    }, 0);
+
+    console.log(naftaResult);
+    console.log(naftaTotal);
+
+    // Fueloil
+    const fueloilEf = scopeData[3];
+    const fueloilConst = [fueloilEf.co2, fueloilEf.ch4, fueloilEf.n2o];
+    const fueloilState = newState.fueloil;
+    const fueloilResult = [];
+
+    fueloilConst.map((item) => {
+        return fueloilResult.push(Math.round((item * fueloilState) * 1000) / 1000)
+    });
+
+    const fueloilTotal = fueloilResult.reduce((acc, val) => {
+        return acc + val
+    }, 0);
+
+    console.log(fueloilResult);
+    console.log(fueloilTotal);
+
+    // LPG
+    const lpgEf = scopeData[4];
+    const lpgConst = [lpgEf.co2, lpgEf.ch4, lpgEf.n2o];
+    const lpgState = newState.lpg;
+    const lpgResult = [];
+
+    lpgConst.map((item) => {
+        return lpgResult.push(Math.round((item * lpgState) * 1000) / 1000)
+    });
+
+    const lpgTotal = lpgResult.reduce((acc, val) => {
+        return acc + val
+    }, 0);
+
+    console.log(lpgResult);
+    console.log(lpgTotal);
+
+
+    // Resultado Scope 1 
+    const scope1 = Math.round((gasTotal + gasoilTotal + naftaTotal + fueloilTotal + lpgTotal) * 100) / 100;
+    console.log(`Scope 1 = ${scope1} kg CO2e`);
+
 
     return (
         <div style={{ display: "flex" }}>
@@ -27,7 +116,7 @@ export const Results = () => {
 
                 </div>
                 <div className='down'>
-                    <p>{data}</p>
+                    <p>Scope 1 = {scope1} Kg CO2e </p>
                 </div>
             </div>
         </div>
